e27dbae58ea2335c8de4b47ad080fbf6b0ff5688
Tek <tek@randhome.io>
2019-01-31 04:06:56 +0100

    update setup

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='pysafe',
     version='0.1.1',
     description='Google Safe Browsing API python wrapper',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/Te-k/pysafebrowsing',
     author='Tek',
     author_email='tek@randhome.io',
@@ -13,5 +18,11 @@ setup(
     packages=['pysafebrowsing'],
     entry_points= {
         'console_scripts': [ 'safebrowsing=pysafebrowsing.cli:main' ]
-    }
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+
 )
