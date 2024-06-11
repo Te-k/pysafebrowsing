951ddcdf3117d5061e71027871fc4c83c2d119ba
tek <tek@randhome.io>
2024-06-11 10:39:48 +0200

    Update requests version

--- setup.py
+++ setup.py
@@ -19,7 +19,10 @@ setup(
     author="Tek",
     author_email="tek@randhome.io",
     keywords="security",
-    install_requires=["requests", "configparser"],
+    install_requires=[
+        "requests>=2.32.3",
+        "configparser"
+    ],
     license="MIT",
     packages=["pysafebrowsing"],
     entry_points={"console_scripts": ["safebrowsing=pysafebrowsing.cli:main"]},
